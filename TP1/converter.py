91ac8a1684f3d68a75e21197359caba8773d81cc
tiagoribeiro2001 <ribeiro.tiago2001@gmail.com>
2022-03-14 23:56:21 +0000

    Yacc

--- TP1/converter.py
+++ TP1/converter.py
@@ -1,48 +1,31 @@
 from genericpath import exists
 from re import T
 import ply.lex as lex
+import ply.yacc as yacc
 import sys
 import csv
 import json
 
-# def readCSV():
-#     print(sys.argv[1])
-#     if exists(sys.argv[1]):
-#         file = open(sys.argv[1])
-#         print("teste")
-#         for row in file:
-#             print(row)
-#     else:
-#         print("Couldn't read CSV file.")
-        
-
-# readCSV()
-
-
-tokens = ["CA", "CF", "VG", "NUM", "FA", "ID"]
+tokens = ["COMA", "LBRACE", "RBRACE", "DCOLON", "INT", "ID"]
+# VG -> Vírgula
 # CA -> Chaveta aberta
 # CF -> Chaveta fechada
-# VG -> Vírgula
-# NUM -> Números
-# FA -> Funções de Agregação
+# DP -> Dois pontos (::)
+# NUM -> Número
 # ID -> Identificadores
 
+t_COMA = r','
+t_LBRACE = r'{'
+t_RBRACE = r'}'
+t_DCOLON = r'::'
 
-t_CA = r'\{'
-t_CF = r'\}'
-t_VG = r','
-
-def t_NUM(t):
+def t_INT(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-def t_FA(t):
-    r'::\w+'
-    return t
-
 def t_ID(t):
-    r'[a-zA-ZÀÁÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ_][\w/]*'
+    r'[a-zA-Z_][\w/]*'
     return t
 
 # carateres a ignorar
@@ -55,8 +38,60 @@ def t_error(t):
 # analisador léxico
 lexer = lex.lex()
 
+def p_language(p):
+    '''
+    language : expression
+                | interval
+                | size
+                | function
+                | empty
+    '''
+    print(p[1])
 
-for line in sys.stdin:
-    lexer.input(line)
-    for tok in lexer:
-        print(tok)
+def p_expression(p):
+    '''
+    expression : ID COMA
+    '''
+    p[0] = p[1]
+
+def p_interval(p):
+    '''
+    interval : LBRACE INT COMA INT RBRACE
+    '''
+    p[0] = (p[2], p[4])
+
+def p_size(p):
+    '''
+    size : LBRACE INT RBRACE
+    '''
+    p[0] = p[2]
+
+def p_function(p):
+    '''
+    function : DCOLON ID
+    '''
+    p[0] = p[2]
+
+def p_empty(p):
+    '''
+    empty :
+    '''
+    p[0] = None
+
+parser = yacc.yacc()
+
+def readCSV():
+    fileName = sys.argv[1]
+    if exists(fileName):
+        file = open(fileName)
+        firstLine = file.readline()
+        parser.parse(firstLine)
+    else:
+        print("Couldn't read CSV file.")
+
+readCSV()        
+
+# for line in sys.stdin:
+#     lexer.input(line)
+#     for tok in lexer:
+#         print(tok)
